cbb00992ecdf17f32dc5fc434c079608d0923dc3
zhangjiamin <jiaminzh@qq.com>
2021-12-01 21:30:24 +0800

    update simple connect the client with ssh

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module go-terminal
 
 go 1.17
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
--- terminal01/client.go
+++ terminal01/client.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"time"
+)
+
+func main()  {
+	sshHost := "192.168.127.128"
+	sshUser := "jamie"
+	sshPasswrod := "root"
+	sshType := "password"  // password或者key
+	//sshKeyPath := "" // ssh id_rsa.id路径
+	sshPort := 22
+
+	// 创建ssh登录配置
+	config := &ssh.ClientConfig{
+		Timeout: time.Second, // ssh连接time out时间一秒钟,如果ssh验证错误会在一秒钟返回
+		User: sshUser,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),  // 这个可以,但是不够安全
+		//HostKeyCallback: hostKeyCallBackFunc(h.Host),
+	}
+	if sshType == "password" {
+		config.Auth = []ssh.AuthMethod{ssh.Password(sshPasswrod)}
+	} else {
+		//config.Auth = []ssh.AuthMethod(publicKeyAuthFunc(sshKeyPath))
+		return
+	}
+
+	// dial 获取ssh client
+	addr := fmt.Sprintf("%s:%d",sshHost,sshPort)
+	sshClient,err := ssh.Dial("tcp",addr,config)
+	if err != nil {
+		log.Fatal("创建ssh client 失败",err)
+	}
+	defer sshClient.Close()
+
+	// 创建ssh-session
+	session,err := sshClient.NewSession()
+	if err != nil {
+		log.Fatal("创建ssh session失败",err)
+	}
+
+	defer session.Close()
+
+	// 执行远程命令
+	combo,err := session.CombinedOutput("whoami; cd /; ls -al;")
+	if err != nil {
+		log.Fatal("远程执行cmd失败",err)
+	}
+	log.Println("命令输出:",string(combo))
+}
+
+//func publicKeyAuthFunc(kPath string) ssh.AuthMethod  {
+//	keyPath ,err := homedir.Expand(kPath)
+//	if err != nil {
+//		log.Fatal("find key's home dir failed",err)
+//	}
+//
+//	key,err := ioutil.ReadFile(keyPath)
+//	if err != nil {
+//		log.Fatal("ssh key file read failed",err)
+//	}
+//
+//	signer,err := ssh.ParsePrivateKey(key)
+//	if err != nil {
+//		log.Fatal("ssh key signer failed",err)
+//	}
+//	return ssh.PublicKeys(signer)
+//}
